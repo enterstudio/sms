e12a3dbf4538d7c3adb76f7ad4082969570f7c18
chiara <cchiappini@ebay.com>
2015-03-15 08:27:00 +0000

    added clean build in the script

--- apiclient/src/test/java/com/sms/meal/backend/ParseMealParserTest.java
+++ apiclient/src/test/java/com/sms/meal/backend/ParseMealParserTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ParseMealParserTest {
     @Test
